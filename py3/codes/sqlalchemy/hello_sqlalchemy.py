54e98d8d9589dd12bd3bc379d8c9555bf15c9108
Hosung Lee <hs.gg.lee@gmail.com>
2022-05-18 14:46:22 +0900

    Modifies sqlalchemy example.

--- py3/codes/sqlalchemy/hello_sqlalchemy.py
+++ py3/codes/sqlalchemy/hello_sqlalchemy.py
@@ -1,18 +1,4 @@
-from sqlalchemy import create_engine
-
-class HelloRepository:
-    def __init__(self) -> None:
-        self._db_url = "sqlite:///hello.db"
-        self._engine = None
-
-    def connect_to_db(self) -> None:
-        pass
-
-    def close_db_connection(self) -> None:
-        pass
-
+import sqlalchemy
 
 if __name__ == "__main__":
-    repo = HelloRepository()
-    repo.connect_to_db()
-    repo.close_db_connection()
+    print("hello sqlalchemy:", sqlalchemy.__version__)
