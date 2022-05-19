72aea600c780a83a489a0da74b261cdac4c1423b
Hosung Lee <hs.gg.lee@gmail.com>
2022-05-19 09:54:18 +0900

    Adds collection example.

---
+++ py3/codes/collections/convert_string_representation_list_to_list.py
@@ -0,0 +1,25 @@
+# https://stackoverflow.com/questions/1894269/how-to-convert-string-representation-of-list-to-a-list
+
+import ast
+import json
+
+x = '[ "A","B","C" , " D"]'
+x_list = ast.literal_eval(x)
+print(x_list, type(x_list))
+
+x_list = json.loads(x)
+print(x_list, type(x_list))
+
+x = '["foo",44,null]'
+try:
+    x_list = ast.literal_eval(x)
+    print(x_list, type(x_list))
+except Exception as ex:
+    print(ex)
+
+x = '["foo",44,null]'
+try:
+    x_list = json.loads(x)
+    print(x_list, type(x_list))
+except Exception as ex:
+    print(ex)
